a835fa1d14885183624fc70e6d1ccde2b0cb4beb
Oleksii Mostovyi Jr <91433946+omostovyijr@users.noreply.github.com>
2021-11-24 10:20:43 +0200

    Task 1

--- 1-array.js
+++ 1-array.js
@@ -5,4 +5,10 @@ const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 const first = ages[0];
 const last = ages[ages.length - 1];
 
-console.log({ first, last });
+ages.push(22); // Add at the end of array
+ages.unshift(11); // Add at the top of array
+
+ages.pop(0); // Remove at the end of array
+ages.shift() // Remove at the top of array
+
+console.log({ages});
